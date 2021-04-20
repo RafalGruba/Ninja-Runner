77a000b4c63d766b398fb4fca9dd4dc62fb0cf7d
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-04-20 14:00:40 +0200

    LevelExit
    -added sprite
    -added boxCollider2D
    -added LevelExit.cs - after player jump in in collider range UI pop-outs, slow-mo is activated for 3 seconds to slow down our Ninja, then time freezes

---
+++ Projekt1/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float myTimeScale = 0.1f;
+    [SerializeField] float timePassing = 0.1f;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LevelComplete());
+    }
+
+
+    IEnumerator LevelComplete()
+    {
+        Time.timeScale = myTimeScale;
+        Debug.Log("slow-mo is set");
+        yield return new WaitForSeconds(timePassing);
+        Time.timeScale = 0f;
+        Debug.Log("Level has been complete. This Debug will be changed to Load UI where you can see your score, go to main menu, next level or restart level, etc.");
+    }
+}
